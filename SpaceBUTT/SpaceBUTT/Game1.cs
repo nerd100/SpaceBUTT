b3b1148bdda603659d6176ce36d1cd7e1a42f261
nerd100 <den.meyer1@web.de>
2015-05-29 14:10:41 +0200

    keyboard controls WASD

--- SpaceBUTT/SpaceBUTT/Game1.cs
+++ SpaceBUTT/SpaceBUTT/Game1.cs
@@ -18,32 +18,32 @@ namespace SpaceBUTT
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
        
-        //some shit
-       public Matrix View;
+       
+        public Matrix View;
         public Matrix Projection;
+        public Vector3 modelPosition = new Vector3(0, 0, 0);
+        public GraphicsDevice device;
+
         Model myModel;
-        //stuff
-        float aspectRatio;
+            
         Vector3 cameraPosition = new Vector3(0.0f, 1500.0f, 5000.0f);
         Vector3 cameraView = new Vector3(0.0f, 0.0f, 0.0f);
         Vector3 modelVelocity = new Vector3(0, 0, 0);
-        public Vector3 modelPosition = new Vector3(0, 0, 0);
-       public GraphicsDevice device;
+        
+        float aspectRatio;
         float modelRotationZ = 0.0f;
         float modelRotationX = 0.0f;
         float modelSpeed = 5.0f;
         float screenWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
         float screenHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
 
-        
-       
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-
         }
+
 
         protected override void Initialize()
         {
@@ -57,8 +57,7 @@ namespace SpaceBUTT
 
       
         protected override void LoadContent()
-        {
-     
+        {     
             spriteBatch = new SpriteBatch(GraphicsDevice);
             myModel = Content.Load<Model>("Ship1");
             aspectRatio = graphics.GraphicsDevice.Viewport.AspectRatio;
@@ -66,7 +65,6 @@ namespace SpaceBUTT
             View = Matrix.CreateLookAt(cameraPosition, cameraView, Vector3.Up);    
         
         }
-
 
 
         protected override void UnloadContent()
@@ -77,15 +75,13 @@ namespace SpaceBUTT
      
         protected override void Update(GameTime gameTime)
         {
-        
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
+            
             UpdateInput();
-
-            // Add velocity to the current position.
+                     
             modelPosition += modelVelocity;
-
-            // Bleed off velocity over time.
+     
             modelVelocity *= 0.95f;
             modelRotationX *= 0.95f;
             modelRotationZ *= 0.95f;
@@ -95,39 +91,70 @@ namespace SpaceBUTT
             base.Update(gameTime);
         }
 
-        protected void UpdateInput()
-        {        
 
+        protected void UpdateInput()
+        {
+            KeyboardState stat = Keyboard.GetState();
+            Vector3 modelVelocityX = Vector3.Zero;
+            Vector3 modelVelocityY = Vector3.Zero;
+
+            //Xbox controls
             GamePadState currentState = GamePad.GetState(PlayerIndex.One);
             if (currentState.IsConnected)
             {
-
-               
                 modelRotationZ -= currentState.ThumbSticks.Left.X * 0.02f;
                 modelRotationX += currentState.ThumbSticks.Left.Y * 0.02f;
-
-                Vector3 modelVelocityX = Vector3.Zero;
-                Vector3 modelVelocityY = Vector3.Zero;
-
+                
                 modelVelocityX.X = modelSpeed;
                 modelVelocityY.Y = modelSpeed;
 
                 modelVelocityX *= currentState.ThumbSticks.Left.X;
                 modelVelocityY *= currentState.ThumbSticks.Left.Y;
 
-
                 modelVelocity += modelVelocityX;
                 modelVelocity += modelVelocityY;
 
-                GamePad.SetVibration(PlayerIndex.One,
+               /* GamePad.SetVibration(PlayerIndex.One,
                     currentState.Triggers.Right,
                     currentState.Triggers.Left);
-
+                */
             }
-            else { 
-            
+            else {
+                if (stat.IsKeyDown(Keys.Escape)) {
+                    this.Exit();
+                }
+                //keyboard controls
+                if (stat.IsKeyDown(Keys.W))
+                {
+                    modelRotationX += 0.02f;
+                    modelVelocityY.Y = modelSpeed;
+                    modelVelocityY *= 1;
+                    modelVelocity += modelVelocityY;
+                }
+                if (stat.IsKeyDown(Keys.A))
+                {
+                    modelRotationZ += 0.02f;
+                    modelVelocityX.X -= modelSpeed;
+                    modelVelocityX *= 1;
+                    modelVelocity += modelVelocityX;                  
+                    
+                }
+                if (stat.IsKeyDown(Keys.S))
+                {
+                    modelRotationX -= 0.02f;
+                    modelVelocityY.Y -= modelSpeed;
+                    modelVelocityY *= 1;
+                    modelVelocity += modelVelocityY;
+                }
+                if (stat.IsKeyDown(Keys.D))
+                {
+                    modelRotationZ -= 0.02f;
+                    modelVelocityX.X = modelSpeed;
+                    modelVelocityX *= 1;
+                    modelVelocity += modelVelocityX;                  
+                }
             }
-
+                //screensize TODO: Was anderes überlegen!
                 if (modelPosition.Y >= screenHeight + 900)
                 {
                     modelPosition.Y = screenHeight + 900;
@@ -143,20 +170,18 @@ namespace SpaceBUTT
                 if (modelPosition.X <= -(screenWidth + 1000))
                 {
                     modelPosition.X  = -(screenWidth + 1000);
-                }
-           
-               
-            
+                }                     
         }
      
+
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
             DrawModel(myModel);
           
-
             base.Draw(gameTime);
         }
+
 
         private void DrawModel(Model myModel)
         {
@@ -170,7 +195,6 @@ namespace SpaceBUTT
                     effect.View = View;
                     effect.Projection = Projection;
                 }
-                // Draw the mesh, using the effects set above.
                 mesh.Draw();
             }
 
